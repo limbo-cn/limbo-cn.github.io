a7ef69f2b03c91c09dbfd1a0beaa3be8ff75f01e
limbo-cn <317091439@qq.com>
2019-07-07 16:02:29 +0800

    add heart

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Proposal />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Proposal from '@/components/Proposal.vue'; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld,
+    Proposal,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue { }
 </script>
